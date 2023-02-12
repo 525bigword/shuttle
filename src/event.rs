5053d2a235f734ede92fe29e23b59a5e6172fba9
hans <837713748@qq.com>
2023-02-12 23:54:28 +0800

    鼠标坐标

--- src/event.rs
+++ src/event.rs
@@ -1,5 +1,31 @@
+use mouse_position::mouse_position::Mouse;
 
+fn a() {
+    let position = Mouse::get_mouse_position();
+    match position {
+        Mouse::Position { x, y } => println!("x: {}, y: {}", x, y),
+        Mouse::Error => println!("Error getting mouse position"),
+    }
+}
 
-fn main(){
-    let position=Mouse::get_mouse_position();
+#[cfg(test)]
+mod tests {
+    use std::{thread, time};
+
+    use super::*;
+
+    #[test]
+    fn test_connect_device() {
+        while true {
+            let position = Mouse::get_mouse_position();
+            match position {
+                Mouse::Position { x, y } => println!("x: {}, y: {}", x, y),
+                Mouse::Error => println!("Error getting mouse position"),
+            }
+            thread::sleep(time::Duration::from_millis(1000))
+        }
+    }
+
+    fn test_dc(){
+    }
 }
