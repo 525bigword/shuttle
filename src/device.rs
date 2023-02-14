5b4dc614b5fae6412c609894cb938776a67ca760
hans <837713748@qq.com>
2023-02-14 21:01:39 +0800

    cross bluetooth

--- src/device.rs
+++ src/device.rs
@@ -1,13 +1,15 @@
+#![allow(clippy::let_unit_value)]
+use bluest::{Adapter, Device, AdvertisingDevice};
 use blurz::bluetooth_adapter::BluetoothAdapter;
 use blurz::bluetooth_device::BluetoothDevice;
 use blurz::bluetooth_discovery_session::BluetoothDiscoverySession;
 use blurz::bluetooth_session::BluetoothSession;
 use std::mem::MaybeUninit;
 use std::sync::Once;
-use std::time::Duration;
 use std::thread;
+use std::time::Duration;
 
-pub fn scan_device(){
+pub fn scan_device() {
     let bt_session = &BluetoothSession::create_session(None).unwrap();
     let adapter: BluetoothAdapter = BluetoothAdapter::init(bt_session).unwrap();
     let adapter_id = adapter.get_id();
@@ -60,17 +62,49 @@ fn disconnect_device(device: &BluetoothDevice) {
 }
 
 // 获取蓝牙
-pub fn lock_device(device: String) -> &'static String{
-   static mut DEVICE:MaybeUninit<String> = MaybeUninit::uninit();
-   static LOCK:Once=Once::new();
+pub fn lock_device(device: String) -> &'static String {
+    static mut DEVICE: MaybeUninit<String> = MaybeUninit::uninit();
+    static LOCK: Once = Once::new();
     LOCK.call_once(|| unsafe {
         DEVICE.as_mut_ptr().write(device);
     });
-    unsafe{&*DEVICE.as_ptr()}
+    unsafe { &*DEVICE.as_ptr() }
 }
+
+
+use tokio_stream::StreamExt;
+pub async  fn a()  -> Result<(),&'static str>{
+    let adapter = Adapter::default()
+            .await
+            .ok_or("Bluetooth adapter not found").unwrap();
+        adapter.wait_available().await;
+
+        println!("starting scan");
+        let mut scan = adapter.scan(&[]).await.unwrap();
+        println!("scan started");
+        Ok(while let Some(discovered_device) = scan.next().await {
+            println!("{:?}",discovered_device);
+            // println!(
+            //     "{}{}: {:?}",
+            //     discovered_device
+            //         .device
+            //         .name()
+            //         .as_deref()
+            //         .unwrap_or("(unknown)"),
+            //     discovered_device
+            //         .rssi
+            //         .map(|x| format!(" ({}dBm)", x))
+            //         .unwrap_or_default(),
+            //     discovered_device.adv_data.services
+            // );
+        })
+}
+
 
 #[cfg(test)]
 mod tests {
+    use bluest::Adapter;
+
     use super::*;
 
     #[test]
@@ -84,5 +118,11 @@ mod tests {
         let device: BluetoothDevice =
             connect_device(bt_session, "/org/bluez/hci0/dev_41_42_8C_A9_68_60");
         disconnect_device(&device);
+    }
+
+    #[tokio::test]
+    async fn  test_disconnect_device1(){
+        let a =a().await;
+        println!("a:{:?}",a)
     }
 }
